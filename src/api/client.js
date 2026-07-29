21f511f899b7728fcf74d1ee72b11747766d8c00
Somaiya-0 <sdark.babe@gmail.com>
2026-07-30 04:10:24 +0600

    aa

--- src/api/client.js
+++ src/api/client.js
@@ -4,6 +4,7 @@ export const API_BASE = import.meta.env.VITE_API_BASE || "http://34.239.101.23:8
 
 export const api = axios.create({ baseURL: API_BASE });
 
+
 api.interceptors.request.use((cfg) => {
   const token = localStorage.getItem("access_token");
   if (token) cfg.headers.Authorization = `Bearer ${token}`;
